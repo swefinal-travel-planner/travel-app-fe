451689bad1c81538261a71c55029e3f188c65db4
huutamisme <hohuutam287@gmail.com>
2025-02-26 13:04:41 +0700

    Split modal into seperate file

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { View, Text, Avatar, Button, Card, Colors, Modal, TextField, NumberInput } from "react-native-ui-lib";
-import { TouchableOpacity, ScrollView, KeyboardAvoidingView, Platform, ActionSheetIOS } from "react-native";
-import { GestureHandlerRootView, PanGestureHandler, State } from "react-native-gesture-handler";
-import Animated, { useSharedValue, useAnimatedStyle, withSpring, withTiming } from "react-native-reanimated";
+import { View, Text, Avatar, Card, Colors } from "react-native-ui-lib";
+import { TouchableOpacity, ScrollView, ActionSheetIOS } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useSharedValue, withTiming } from "react-native-reanimated";
 import { StatusBar } from "expo-status-bar";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import * as ImagePicker from "expo-image-picker";
+import EditProfileModal from "@/components/EditProfileModal";
 
 const ProfileScreen = () => {
     const [modalVisible, setModalVisible] = useState(false);
@@ -13,12 +14,8 @@ const ProfileScreen = () => {
     const [name, setName] = useState("채수빈")
     const [email, setEmail] = useState("csb@gmail.com");
     const [phone, setPhone] = useState("4060001290");
-    const [selectedInfo, setSelectedInfo] = useState("Edit name");
+    const [selectedField, setSelectedField] = useState("Edit name");
     const [profilePic, setProfilePic] = useState(require("@/assets/images/alligator.jpg"));
-    const [tempName, setTempName] = useState(name);
-    const [tempEmail, setTempEmail] = useState(email);
-    const [tempPhone, setTempPhone] = useState(phone);
-    const [errors, setErrors] = useState("");
 
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -65,32 +62,13 @@ const ProfileScreen = () => {
         );
     };
 
-    const validateInputs = (field, value) => {
-        let error = "";
-        if (field.includes("name") && !value.trim()) {
-            error = "Name cannot be empty";
-        } else if (field.includes("phone") && !/^[0-9]{10}$/.test(value)) {
-            error = "Phone must be 10 digits";
-        } else if (field.includes("email") && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
-            error = "Invalid email format";
-        }
-        setErrors(error);
-    };
-
-    const isValid = !errors; // button disabled control
-
-    const handleSave = () => {
-        setName(tempName);
-        setEmail(tempEmail);
-        setPhone(tempPhone);
-        setErrors("");
+    const handleSave = (value, field) => {
+        field.includes("name") ? setName(value) : field.includes("phone") ? setPhone(value) : setEmail(value);
         closeModal();
     };
 
     const openModal = (field) => {
-        setSelectedInfo(field);
-        field.includes("name") ? setTempName(name) : field.includes("phone") ? setTempPhone(phone) : setTempEmail(email);
-        setErrors("");
+        setSelectedField(field);
         setModalVisible(true);
         translateY.value = withTiming(0, { duration: 200 }); // Hiển thị modal từ dưới lên
     };
@@ -100,30 +78,12 @@ const ProfileScreen = () => {
         setTimeout(() => setModalVisible(false), 100);
     };
 
-    // Cập nhật `translateY` khi người dùng kéo modal
-    const onGestureEvent = (event) => {
-        translateY.value = event.nativeEvent.translationY;
-    };
-
-    const onHandlerStateChange = (event) => {
-        if (event.nativeEvent.state === State.END) {
-            if (event.nativeEvent.translationY > 100) {
-                closeModal(); // Nếu kéo xuống quá 100px, đóng modal
-            } else {
-                translateY.value = withSpring(0); // Nếu kéo nhẹ, trả modal về vị trí cũ
-            }
-        }
-    };
-
-    // Tạo animation cho modal
-    const animatedStyle = useAnimatedStyle(() => ({
-        transform: [{ translateY: translateY.value }],
-    }));
-
     return (
         <>
             <GestureHandlerRootView style={{ flex: 1, backgroundColor: Colors.grey80, paddingTop: 30 }}>
                 <ScrollView contentContainerStyle={{ padding: 20 }}>
+
+                    {/* Avatar and name */}
                     <View center>
                         <TouchableOpacity onPress={() => openActionSheet()} style={{ borderWidth: 5, borderColor: "green", borderRadius: 100, padding: 3 }}>
                             <Avatar size={120} source={profilePic} />
@@ -202,57 +162,17 @@ const ProfileScreen = () => {
                 </ScrollView>
 
                 {/* Popup modal */}
-                <Modal visible={modalVisible} animationType="slide" transparent>
-                    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{ flex: 1 }}>
-                        <PanGestureHandler onGestureEvent={onGestureEvent} onHandlerStateChange={onHandlerStateChange}>
-                            <Animated.View
-                                style={[
-                                    {
-                                        height: "95%",
-                                        backgroundColor: Colors.white,
-                                        padding: 20,
-                                        borderTopLeftRadius: 20,
-                                        borderTopRightRadius: 20,
-                                        marginTop: "auto",
-                                        justifyContent: "space-between"
-                                    },
-                                    animatedStyle, // Áp dụng animation
-                                ]}
-                            >
-                                {/* Drag Indicator */}
-                                <View centerH marginB-10>
-                                    <View style={{ width: 40, height: 5, backgroundColor: Colors.grey50, borderRadius: 10, marginBottom: 10 }} />
-                                </View>
-
-                                <View flex-1 centerV>
-                                    <Text text30 style={{ fontWeight: "bold" }} center>{selectedInfo.includes("name") ? "Edit your name" : selectedInfo.includes("phone") ? "Edit your phone number" : "Edit your email"}</Text>
-                                    <TextField
-                                        marginV-20
-                                        padding-5
-                                        autoFocus
-                                        keyboardType={selectedInfo === "Edit phone number" ? "number-pad" : "default"}
-                                        containerStyle={{ borderWidth: 2, borderRadius: 10, height: 50, justifyContent: "center" }}
-                                        placeholder={selectedInfo === "Edit name" ? "Enter your name" : selectedInfo === "Edit phone number" ? "Enter your phone number" : "Enter your email"}
-                                        value={selectedInfo === "Edit name" ? tempName : selectedInfo === "Edit phone number" ? tempPhone : tempEmail}
-                                        onChangeText={(text) => { selectedInfo === "Edit name" ? setTempName(text) : selectedInfo === "Edit phone number" ? setTempPhone(text) : setTempEmail(text); validateInputs(selectedInfo, text) }}
-                                    />
-                                    <Text text65 red30>{errors}</Text>
-                                </View>
-                                {/* Save button */}
-                                <Button
-                                    label="Save"
-                                    backgroundColor={Colors.green5}
-                                    marginT-20
-                                    disabled={!isValid}
-                                    onPress={handleSave} />
-                            </Animated.View>
-                        </PanGestureHandler>
-                    </KeyboardAvoidingView>
-                </Modal>
+                <EditProfileModal
+                    translateY={translateY}
+                    value={selectedField === "Edit name" ? name : selectedField === "Edit phone number" ? phone : email}
+                    visible={modalVisible}
+                    field={selectedField}
+                    onSave={handleSave}
+                    closeModal={closeModal}
+                />
             </GestureHandlerRootView >
             <StatusBar style="dark" />
         </>
     );
 };
-
 export default ProfileScreen;
