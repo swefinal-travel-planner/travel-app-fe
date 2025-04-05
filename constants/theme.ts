9b4c7a35b2074d98c0138485ca420e4877587385
kreema <kreema1602@gmail.com>
2025-04-05 10:48:07 +0700

    [Theme] add theme

---
+++ constants/theme.ts
@@ -0,0 +1,21 @@
+export const Radius = {
+  NONE: 0,
+  SMALL: 4,
+  NORMAL: 8,
+  ROUNDED: 16,
+  FULL: 9999,
+};
+
+export const Border = {
+  THIN: 1,
+  NORMAL: 2,
+  BOLD: 4,
+};
+
+export const Size = {
+  XS: 24,
+  SM: 32,
+  MD: 48,
+  LG: 64,
+  XL: 96,
+};
