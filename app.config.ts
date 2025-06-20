93bdadbdd6084dbeaced4bfa055776f7748def7b
kreema <kreema1602@gmail.com>
2025-06-20 07:55:29 +0700

    app.config

---
+++ app.config.ts
@@ -0,0 +1,77 @@
+import { ConfigContext, ExpoConfig } from 'expo/config'
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'travel-app-fe',
+  slug: 'travel-app-fe',
+  version: '1.0.0',
+  orientation: 'portrait',
+  scheme: 'myapp',
+  userInterfaceStyle: 'automatic',
+  newArchEnabled: true,
+  platforms: ['android'],
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/icons/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+    package: 'com.qhuongng.travelapp',
+    googleServicesFile: './google-services.json',
+  },
+  plugins: [
+    'expo-router',
+    [
+      'expo-splash-screen',
+      {
+        image: './assets/icons/splash-icon-dark.png',
+        imageWidth: 200,
+        resizeMode: 'contain',
+        backgroundColor: '#ffffff',
+        dark: {
+          image: './assets/icons/splash-icon-light.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#000',
+        },
+      },
+    ],
+    '@react-native-google-signin/google-signin',
+    'expo-localization',
+    [
+      'expo-secure-store',
+      {
+        configureAndroidBackup: true,
+      },
+    ],
+    [
+      '@rnmapbox/maps',
+      {
+        RNMapboxMapsDownloadToken: process.env.EXPO_SECRET_MAPBOX_DOWNLOAD_TOKEN,
+      },
+    ],
+    [
+      'expo-font',
+      {
+        fonts: [
+          'node_modules/@expo-google-fonts/plus-jakarta-sans/400Regular/PlusJakartaSans_400Regular.ttf',
+          'node_modules/@expo-google-fonts/plus-jakarta-sans/400Regular_Italic/PlusJakartaSans_400Regular_Italic.ttf',
+          'node_modules/@expo-google-fonts/plus-jakarta-sans/700Bold/PlusJakartaSans_700Bold.ttf',
+          'node_modules/@expo-google-fonts/plus-jakarta-sans/700Bold_Italic/PlusJakartaSans_700Bold_Italic.ttf',
+        ],
+      },
+    ],
+    'expo-asset',
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    router: {
+      origin: false,
+    },
+    eas: {
+      projectId: '5a3053c6-fc75-46ea-b811-a9eccf5365d3',
+    },
+  },
+  owner: 'qhuongng',
+})
