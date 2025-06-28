eb35c37bee943ed4890d4b1aa4807f8f02aaa3fc
huutamisme <hohuutam287@gmail.com>
2025-06-28 13:46:17 +0700

    remove redundant code

--- components/SpotCards/SpotCard.tsx
+++ components/SpotCards/SpotCard.tsx
@@ -1,36 +1,23 @@
-import { useMemo, useState } from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
-
-import Ionicons from '@expo/vector-icons/Ionicons'
-
 import { FontFamily, FontSize } from '@/constants/font'
 import { colorPalettes } from '@/constants/Itheme'
 import { Radius } from '@/constants/theme'
 import { useThemeStyle } from '@/hooks/useThemeStyle'
-
-import { useRouter } from 'expo-router'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import { useMemo, useState } from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import PressableOpacity from '../PressableOpacity'
-interface Location {
-  long: number
-  lat: number
-}
 
 interface SpotCardProps {
-  id: string
-  location: Location
   name: string
-  properties: string[]
-  type: string[]
   image: string
   isSaved: boolean
+  address: string
 }
 
-const SpotCard: React.FC<SpotCardProps> = ({ id, location, name, properties, type, image, isSaved }) => {
+const SpotCard: React.FC<SpotCardProps> = ({ name, image, address, isSaved }) => {
   const theme = useThemeStyle()
   const styles = useMemo(() => createStyles(theme), [theme])
-
   const [saved, setSaved] = useState(isSaved)
-  const router = useRouter()
 
   return (
     <View style={styles.wrapper}>
@@ -51,7 +38,7 @@ const SpotCard: React.FC<SpotCardProps> = ({ id, location, name, properties, typ
           <Ionicons name="location-outline" size={16} color={theme.text} style={{ marginRight: 4 }} />
 
           <Text style={styles.location} numberOfLines={1}>
-            {location.long}, {location.lat}
+            {address}
           </Text>
         </View>
       </View>
