50a9e29eacc19b9169b8f0cbfb86009514528819
kreema <kreema1602@gmail.com>
2025-07-02 00:16:28 +0700

    Enhance API URL validation and improve error handling in app index (#85)
    - Added a check for BE_URL and CORE_URL in index.tsx to display an error message if they are not set.
    - Updated the fallback for CLOUD_URL in cloudApi.ts to use EMPTY_STRING for better error handling.

--- lib/cloudApi.ts
+++ lib/cloudApi.ts
@@ -1,6 +1,7 @@
 import createAxiosInstance from '@/lib/axios'
+import { EMPTY_STRING } from '@/constants/utilConstants'
 
-const CLOUD_URL = process.env.EXPO_PUBLIC_CLOUD_API_URL ?? 'https://api.cloudinary.com/v1_1'
+const CLOUD_URL = process.env.EXPO_PUBLIC_CLOUD_API_URL ?? EMPTY_STRING
 
 const cloudApi = createAxiosInstance(CLOUD_URL)
 
